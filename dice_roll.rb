4fd823785a715bed314a9af99364849d16af7c2d
Jesse Berman <jesse.ben.berman@gmail.com>
2019-06-12 00:31:00 +0000

    Automatically backed up by Learn

--- dice_roll.rb
+++ dice_roll.rb
@@ -3,5 +3,7 @@
 # Feel free to google "how to generate a random number in ruby"
 
 def roll(num)
-  num = num.floor(rand(7))
+  num = [1,2,3,4,5,6]
+  randnum = num.floor(rand([6]))
+  return randnum
 end
